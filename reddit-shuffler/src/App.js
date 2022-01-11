00503ace809f40e6bb156b245cb0f46d2daa93b9
eelyort <troylee3006@gmail.com>
2022-01-10 19:09:22 -0600

    github pages deploy

--- reddit-shuffler/src/App.js
+++ reddit-shuffler/src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
